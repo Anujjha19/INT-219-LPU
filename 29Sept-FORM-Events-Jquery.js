86664abeae0421dfb42e9551de00f1c3d1b58283
Anuj Jha <anujjha710@gmail.com>
2022-09-29 15:27:50 +0530

    init

---
+++ 29Sept-FORM-Events-Jquery.js
@@ -0,0 +1,20 @@
+
+
+//JQuery Events
+
+$("#p1").mouseup(function(){
+    alert("Mouse up Over p1 !");
+});
+
+$("#p1").hover(function(){
+    alert("You entered p1 !");
+},
+function(){
+    alert("Bye! You now leave p1 !");
+});
+$("input").focus(function(){
+    $(this).css("background-color" ,#);
+});
+$(input).blur(function(){
+    $(this).css("background-color" , #ffffff);
+})
